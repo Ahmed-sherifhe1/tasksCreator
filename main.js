9a38757c288d0341d899bf3e0ee6324d4113f287
Ahmed-sherifhe1 <115046140+Ahmed-sherifhe1@users.noreply.github.com>
2023-07-20 19:35:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+let myInput = document.querySelector ( ".input" ) ;
+let addBtn  = document.querySelector ( ".add"   ) ;
+if(localStorage.tasks!=null){
+    myArr = JSON.parse(localStorage.tasks)
+}
+else{
+    let myArr = []
+}
+let mydiv = document.querySelector(".tasks");
+//addTasks
+myInput.onclick=function(){
+    myInput.style.border="orange"
+}
+addBtn.onclick=function(){
+    if(myInput.value!=""){
+        let myOpj = {
+            "id" : Date.now(),
+            "title":myInput.value,
+        }
+        //push to array
+        myArr.push(myOpj)
+        localStorage.setItem("tasks",JSON.stringify(myArr))
+        showTasks()
+        myInput.value="";
+    }
+}
+//showTasks
+function showTasks(){
+    var k = ``
+    for (let i = 0; i < myArr.length; i++) {
+        
+        k +=`
+        <div>
+        <p>${myArr[i].title}</p>
+        <button onclick = "delTasks(${i})" >Delete</button>
+        </div>
+        `
+    }
+    mydiv.innerHTML=k;
+}
+showTasks()
+//delTasks
+function delTasks(task){
+    myArr.splice(task,1);
+    localStorage.tasks=JSON.stringify(myArr)
+    showTasks()
+}
